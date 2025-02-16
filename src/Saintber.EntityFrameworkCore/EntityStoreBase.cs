33bdfb9bf0c853409d9a550263b8bf05d492b2ed
saintber <saintber@gmail.com>
2025-02-16 23:18:57 +0800

    #92 調整介面方案與增加 dev 發行

--- src/Saintber.EntityFrameworkCore/EntityStoreBase.cs
+++ src/Saintber.EntityFrameworkCore/EntityStoreBase.cs
@@ -4,14 +4,13 @@
     /// 實體資料條件取得存取庫共用基底類別。
     /// </summary>
     /// <typeparam name="TEntity">實體資料型別。</typeparam>
-    /// <typeparam name="TModel">資料模型型別。</typeparam>
     /// <typeparam name="TFilterModel">篩選資料模型型別。</typeparam>
-    public abstract class GetEntityStoreBase<TEntity, TModel, TFilterModel>
-        : IGetEntityStore<TEntity, TModel, TFilterModel>
+    public abstract class EntityStoreBase<TEntity, TFilterModel>
+        : IEntityStore<TEntity, TFilterModel>
     {
-        private readonly IEntityStore<TEntity> entityStore;
+        protected readonly IEntityStore<TEntity> entityStore;
 
-        public GetEntityStoreBase(IEntityStore<TEntity> entityStore)
+        public EntityStoreBase(IEntityStore<TEntity> entityStore)
         {
             this.entityStore = entityStore;
         }
@@ -26,12 +25,26 @@
 
         public abstract Task<IQueryable<TEntity>> GetAsync(TFilterModel model, CancellationToken cancellationToken = default);
 
-        public abstract Task<IEnumerable<TModel>> GetAsync(IQueryable<TEntity> query, CancellationToken cancellationToken = default);
-
         public virtual Task<IQueryable<TEntity>> GetAsync(CancellationToken cancellationToken = default)
             => entityStore.GetAsync(cancellationToken);
 
         public virtual Task UpdateAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default)
             => entityStore.UpdateAsync(entities, cancellationToken);
+    }
+
+    /// <summary>
+    /// 實體資料條件取得存取庫共用基底類別。
+    /// </summary>
+    /// <typeparam name="TEntity">實體資料型別。</typeparam>
+    /// <typeparam name="TModel">資料模型型別。</typeparam>
+    /// <typeparam name="TFilterModel">篩選資料模型型別。</typeparam>
+    public abstract class EntityStoreBase<TEntity, TModel, TFilterModel>
+        : EntityStoreBase<TEntity, TFilterModel>
+        , IEntityStore<TEntity, TModel, TFilterModel>
+    {
+        public EntityStoreBase(IEntityStore<TEntity, TFilterModel> entityStore)
+            : base(entityStore) { }
+
+        public abstract Task<IEnumerable<TModel>> GetAsync(IQueryable<TEntity> query, CancellationToken cancellationToken = default);
     }
 }
--- src/Saintber.EntityFrameworkCore/IEntityStore.cs
+++ src/Saintber.EntityFrameworkCore/IEntityStore.cs
@@ -1,7 +1,7 @@
 ﻿namespace Saintber.EntityFrameworkCore
 {
     /// <summary>
-    /// 實體資料存取庫。
+    /// 實體資料存取庫介面。
     /// </summary>
     /// <typeparam name="TEntity">實體資料型別。</typeparam>
     public interface IEntityStore<TEntity>
@@ -36,5 +36,46 @@
         /// <param name="cancellationToken">取消權杖。</param>
         /// <returns>非同步作業。</returns>
         Task UpdateAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+    }
+
+    /// <summary>
+    /// 實體資料存取庫介面，含條件取得。
+    /// </summary>
+    /// <typeparam name="TEntity">實體資料型別。</typeparam>
+    /// <typeparam name="TFilterModel">篩選資料模型型別。</typeparam>
+    public interface IEntityStore<TEntity, TFilterModel> : IEntityStore<TEntity>
+    {
+        /// <summary>
+        /// 取得符合篩選條件的實體資料。
+        /// </summary>
+        /// <param name="model">篩選資料模型。</param>
+        /// <param name="cancellationToken">取消權杖。</param>
+        /// <returns>符合篩選條件的實體資料清單。</returns>
+        Task<IQueryable<TEntity>> GetAsync(TFilterModel model, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 取得允許存取範圍內，符合篩選條件的實體資料。
+        /// </summary>
+        /// <param name="model">篩選資料模型。</param>
+        /// <param name="cancellationToken">取消權杖。</param>
+        /// <returns>允許存取且符合篩選條件的實體資料清單。</returns>
+        Task<IQueryable<TEntity>> GetAllowAsync(TFilterModel model, CancellationToken cancellationToken = default);
+    }
+
+    /// <summary>
+    /// 實體資料存取庫介面，含條件取得與關聯查詢。
+    /// </summary>
+    /// <typeparam name="TEntity">實體資料型別。</typeparam>
+    /// <typeparam name="TModel">資料模型型別。</typeparam>
+    /// <typeparam name="TFilterModel">篩選資料模型型別。</typeparam>
+    public interface IEntityStore<TEntity, TModel, TFilterModel> : IEntityStore<TEntity, TFilterModel>
+    {
+        /// <summary>
+        /// 由查詢表達式取得資料模型清單。
+        /// </summary>
+        /// <param name="query">查詢表達式。</param>
+        /// <param name="cancellationToken">取消權杖。</param>
+        /// <returns>資料模型清單。</returns>
+        Task<IEnumerable<TModel>> GetAsync(IQueryable<TEntity> query, CancellationToken cancellationToken = default);
     }
 }
