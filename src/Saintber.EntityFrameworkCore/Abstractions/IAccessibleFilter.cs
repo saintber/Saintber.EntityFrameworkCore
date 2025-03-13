fd2c63a9b6295f47c59696a3d4aac2cee1211953
saintber <saintber@gmail.com>
2025-03-13 23:01:22 +0800

    附加新增通用介面

--- src/Saintber.EntityFrameworkCore/Abstractions/IAccessibleFilter.cs
+++ src/Saintber.EntityFrameworkCore/Abstractions/IAccessibleFilter.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// 定義篩選器介面，用於根據使用者的存取權限範圍，篩選可存取的資料項目。
     /// </summary>
-    /// <typeparam name="T">要篩選的資料類型。</typeparam>
+    /// <typeparam name="T">要篩選的資料型別。</typeparam>
     public interface IAccessibleFilter<T>
     {
         /// <summary>
@@ -13,5 +13,22 @@
         /// <param name="cancellationToken">可選的取消標記，用於取消非同步操作。</param>
         /// <returns>篩選後的 <see cref="IQueryable{T}"/> 查詢結果。</returns>
         Task<IQueryable<T>> WhereAsync(IQueryable<T> query, CancellationToken cancellationToken = default);
+    }
+
+    /// <summary>
+    /// 定義篩選器介面，用於根據使用者的存取權限範圍，篩選可存取的資料項目。
+    /// </summary>
+    /// <typeparam name="T">要篩選的資料型別。</typeparam>
+    /// <typeparam name="TFilter">額外的篩選條件型別。</typeparam>
+    public interface IAccessibleFilter<T, TFilter>
+    {
+        /// <summary>
+        /// 根據使用者的存取權限，篩選符合條件的資料項目。
+        /// </summary>
+        /// <param name="query">待篩選的查詢物件。</param>
+        /// <param name="filter">篩選條件，用於進一步限制查詢結果。</param>
+        /// <param name="cancellationToken">可選的取消標記，用於取消非同步操作。</param>
+        /// <returns>篩選後的 <see cref="IQueryable{T}"/> 查詢結果。</returns>
+        Task<IQueryable<T>> WhereAsync(IQueryable<T> query, TFilter filter, CancellationToken cancellationToken = default);
     }
 }
--- src/Saintber.EntityFrameworkCore/Abstractions/IAssignedFilter.cs
+++ src/Saintber.EntityFrameworkCore/Abstractions/IAssignedFilter.cs
@@ -14,4 +14,21 @@
         /// <returns>篩選後的 <see cref="IQueryable{T}"/> 查詢結果。</returns>
         Task<IQueryable<T>> WhereAsync(IQueryable<T> query, CancellationToken cancellationToken = default);
     }
+
+    /// <summary>
+    /// 定義一個篩選器介面，用於篩選「已授權」的資料項目。
+    /// </summary>
+    /// <typeparam name="T">要篩選的資料類型。</typeparam>
+    /// <typeparam name="TFilter">額外的篩選條件型別。</typeparam>
+    public interface IAssignedFilter<T, TFilter>
+    {
+        /// <summary>
+        /// 根據使用者被授予的權限，篩選可存取的資料項目。
+        /// </summary>
+        /// <param name="query">待篩選的查詢物件。</param>
+        /// <param name="filter">篩選條件，用於進一步限制查詢結果。</param>
+        /// <param name="cancellationToken">可選的取消標記，用於取消非同步操作。</param>
+        /// <returns>篩選後的 <see cref="IQueryable{T}"/> 查詢結果。</returns>
+        Task<IQueryable<T>> WhereAsync(IQueryable<T> query, TFilter filter, CancellationToken cancellationToken = default);
+    }
 }
